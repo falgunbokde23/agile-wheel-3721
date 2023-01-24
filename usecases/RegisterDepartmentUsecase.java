b02a0c597fd5322ed2883146752346eaf7025285
Falgun Bokde <101393483+falgunbokde23@users.noreply.github.com>
2023-01-24 11:51:59 +0530

    Update RegisterDepartmentUsecase.java
    update

--- usecases/RegisterDepartmentUsecase.java
+++ usecases/RegisterDepartmentUsecase.java
@@ -8,27 +8,6 @@ import com.masai.dao.DepartmentDaoImpl;
 
 public class RegisterDepartmentUsecase {
 	
-//	public static void main(String[] args) {
-//		Scanner sc =new Scanner(System.in);
-//		
-//		System.out.println("Enter Department Name :");
-//		String dname=sc.next();
-//		
-//		System.out.println("Enter Department DNO : ");
-//		int dNO=sc.nextInt();
-//		
-//		
-//		DepartmentDao dao = new DepartmentDaoImpl();
-//		
-//		Department department = new Department();
-//		
-//		department.setDname(dname);
-//		department.setDNO(dNO);
-//		
-//		String result = dao.registerDepartment(department);
-//		
-//		System.out.println(result);
-//	}
 	
 	public static void register() {
 		Scanner sc =new Scanner(System.in);
