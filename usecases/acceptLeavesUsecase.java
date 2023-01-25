8ac727b23494d83a830cdb461832db6b08f7b4d9
Falgun Bokde <101393483+falgunbokde23@users.noreply.github.com>
2023-01-25 11:13:12 +0530

    Update acceptLeavesUsecase.java
    deleted unwanted code

--- usecases/acceptLeavesUsecase.java
+++ usecases/acceptLeavesUsecase.java
@@ -7,19 +7,6 @@ import com.masai.dao.EmployeeDaoImpl;
 
 public class acceptLeavesUsecase {
 	
-	public static void main(String[] args) {
-		
-//			Scanner sc = new Scanner(System.in);
-//			
-//			System.out.println("Enter Employee Id  : ");
-//			int empid=sc.nextInt();
-//			
-//			EmployeeDao dao = new EmployeeDaoImpl();
-//			
-//			String result = dao.acceptLeaves(empid);
-//			
-//			System.out.println(result);
-	}
 
 	
 	public static void acceptLeaves() {
